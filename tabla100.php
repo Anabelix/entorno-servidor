f5c13ea135d07a26bf2b82ab0b17c555f984ffe9
Anabelix <anabelpedrajas@hotmail.com>
2022-09-16 18:40:53 +0200

    cosas

--- tabla100.php
+++ tabla100.php
@@ -1,25 +1,45 @@
 <?php 
     $num=10;
-    $aux=1;
+    $aux=0;
+    $esPrimo=true;
     
-    if ($aux%2==0) 
-        echo "<td>caca</td>"
-    ;
+    function esPrimo ($n) {
+    	if ($n == 0 || $n==1) {
+    		$esPrimo == true;
+    	} else {
+    		$esPrimo==true;
+    		$c=2;
+    		while ($esPrimo && $c <= ($n/2)) {
+    			if ($n%$c==0) {
+    			$esPrimo==false;
+    			}
+    		}
+    	}
+    	return $esPrimo;
+    }
 ?>
 <html>
     <head>
         <title>Tabla Html</title>
         <style>
             table, tr, td { border: 1px solid black; border-collapse: collapse; text-align: center;}
+            table {
+            width: 50%;
+            margin: 0 auto;
+            }
+            
+            .esprimo {
+            	background-color: #87c7ff;
+            }
         </style>
     </head>
     <body>
         <table>
             <?php for($i=0;$i<$num;$i++) { ?>
                 <tr>
-                    <?php for($j=1;$j<$num;$j++) { ?>
-                        <td><?=$aux?></td>
-                        <?php $aux++; ?>
+                    <?php for($j=0;$j<$num;$j++) { ?>
+                        <td <?=(esPrimo ($aux)) ?'class="esprimo"';''?> ><?=$aux?></td>
+                        <?php $aux++;?>
                     <?php } ?>
                 </tr>
             <?php } ?>
