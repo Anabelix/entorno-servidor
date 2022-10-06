e2f082c5d357514f279078b48deb47ee7339218d
anabelix <anabelpedrajas@hotmail.com>
2022-10-06 11:48:47 +0200

    Hash contraseña

--- practicas/hash.php
+++ practicas/hash.php
@@ -1,6 +1,25 @@
 <?php
+
+    set_time_limit(300);
     $cosa = "$2y$10$0GNiidCkeO/VBBHPH0DP6e5tgz6l/FIOxs1RcFloJrXuTYmmAsW72";
+    
+    // echo password_hash($valor, PASSWORD_DEFAULT)."\n";
+    $descubierta=false;
+    while ($descubierta==false) {
+        for ($i=97; $i<123; $i++) {
+            for ($j=97; $j<123; $j++) {
+                for ($k=97; $k<123; $k++) {
+                    for ($l=97; $l<123; $l++) {
+                        $palabra=chr($i).chr($j).chr($k).chr($l);
+                        
+                        if (password_verify($palabra, PASSWORD_DEFAULT)) {
+                            $descubierta = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-
-    echo password_hash($valor, PASSWORD_DEFAULT)."\n";
+    echo $palabra;
 ?>
