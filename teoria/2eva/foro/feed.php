fa30b3232fd4cb61c5de6047302c7e83393f33fe
anabelix <anabelpedrajas@hotmail.com>
2023-01-09 15:14:38 +0100

    Subiendo cosas foro

---
+++ teoria/2eva/foro/feed.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="feed.css">
+    <title>Timeline</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1>Inicio</h1>
+        <form action="" method="post">
+            <div class="cuerpo">
+                <div class="foto" id="admin"></div>
+                <textarea name="tweet" id="tweet" cols="30" rows="10" placeholder="¿Qué está pasando?"></textarea>
+            </div>
+            <input type="submit" value="Twittear">
+        </form>
+    </div>
+</body>
+</html>
